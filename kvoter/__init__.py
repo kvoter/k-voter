1ecbc688c03b917f34edaa836e8bc8fc98ea789e
Madda <nobody.youknow@local>
2014-09-22 11:47:19 +0100

    Remove incorrect comma

--- kvoter/__init__.py
+++ kvoter/__init__.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
                                    password='admin')
         db.session.commit()
 
-    app.config["SECRET_KEY"] = ("I AM THE DEVELOPMENT SECRET KEY!",
+    app.config["SECRET_KEY"] = ("I AM THE DEVELOPMENT SECRET KEY!"
                                 "DO NOT COMMIT ME TO PRODUCTION")
     app.config["DEBUG"] = True
     app.run()
